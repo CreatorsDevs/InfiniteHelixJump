fc4c94d94ef8bb554ba365c6b7841026b6cb1370
CreatorsDevs <creatorsdevs@gmail.com>
2025-01-19 02:32:50 +0530

    Added Player and Consistent Player Jump Script

---
+++ Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    [SerializeField] private float jumpForce;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("RingPlatform"))
+        {
+            rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+}
