419913c33189a37f75ab8c73eeaffe75b413a095
CreatorsDevs <creatorsdevs@gmail.com>
2025-01-20 01:21:32 +0530

    Fixed the glitch regarding the proper generation of first and following helix

--- Assets/Scripts/HelixGenerator.cs
+++ Assets/Scripts/HelixGenerator.cs
@@ -1,33 +1,41 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class HelixGenerator : MonoBehaviour
 {
-    [SerializeField] private GameObject baseRing;
     [SerializeField] private RingPool ringPool;
     [SerializeField] private int numberOfRingPlatforms;
 
     private List<GameObject> pooledObjects;
-    bool generatedFirstHelix = false;
+    private bool generatedFirstHelix = false;
+    private int randomIndex;
 
-    private void Start()
+    IEnumerator Start()
     {
         pooledObjects = ringPool.GetPool();
+        yield return null;
+        if(!generatedFirstHelix && pooledObjects != null)
+            GenerateFirstHelix();
         GenerateHelix();
     }
 
-    private void GenerateHelix()
+    private void GenerateFirstHelix()
     {
-        if (!generatedFirstHelix)
+        GameObject baseRing = GetBaseRingFromPool();
+        if (baseRing != null)
         {
             Vector3 pos = new Vector3(0, 0, 0);
-            Quaternion rotation = Quaternion.Euler(0, UnityEngine.Random.Range(0, -180), 0);
-            Instantiate(baseRing, pos, rotation);
+            Quaternion rotation = Quaternion.Euler(0, UnityEngine.Random.Range(-25, -180), 0);
+            baseRing.transform.SetLocalPositionAndRotation(pos, rotation);
+            baseRing.SetActive(true);
             generatedFirstHelix = true;
         }
-
-        if(generatedFirstHelix)
-        { 
+    }
+    private void GenerateHelix()
+    {
+        if (generatedFirstHelix)
+        {
             for (int i = 1; i < numberOfRingPlatforms; i++)
             {
                 GameObject selectedRingPlatform = GetRandomPlatform();
@@ -41,14 +49,30 @@ public class HelixGenerator : MonoBehaviour
         }
     }
 
+    private GameObject GetBaseRingFromPool()
+    {
+        foreach (GameObject obj in pooledObjects)
+        {
+            if (!obj.activeInHierarchy && obj.CompareTag("BaseRing"))
+                return obj;
+        }
+        return null;
+    }
+
     private GameObject GetRandomPlatform()
     {
         if(pooledObjects.Count > 0)
         {
-            int randomIndex = UnityEngine.Random.Range(0, pooledObjects.Count);
-            GameObject platform = pooledObjects[randomIndex];
-            pooledObjects.RemoveAt(randomIndex);
-            return platform;
+            for(int i=0; i<pooledObjects.Count; i++)
+            {
+                randomIndex = UnityEngine.Random.Range(0, pooledObjects.Count);
+                if (!pooledObjects[randomIndex].activeInHierarchy)
+                {
+                    GameObject platform = pooledObjects[randomIndex];
+                    pooledObjects.RemoveAt(randomIndex);
+                    return platform;
+                }
+            }
         }
         return null;
     }
