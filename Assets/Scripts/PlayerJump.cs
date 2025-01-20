85776d56a00f69763101fac3f74d145deb07ce0b
CreatorsDevs <creatorsdevs@gmail.com>
2025-01-20 14:28:04 +0530

    Added Splash Prefab for ball bouncing effect on the surface

--- Assets/Scripts/PlayerJump.cs
+++ Assets/Scripts/PlayerJump.cs
@@ -1,8 +1,11 @@
+using System.Collections;
+using System.Drawing;
 using UnityEngine;
 
 public class PlayerJump : MonoBehaviour
 {
     [SerializeField] private float jumpHeight;
+    [SerializeField] private GameObject splashPrefab;
     private Rigidbody rb;
 
     void Start()
@@ -16,6 +19,33 @@ public class PlayerJump : MonoBehaviour
         {
             float jumpVelocity = Mathf.Sqrt(2 * Mathf.Abs(Physics.gravity.y) * jumpHeight);
             rb.velocity = new (rb.velocity.x, jumpVelocity, rb.velocity.z);
+
+            if(collision.contactCount > 0)
+            {
+                Vector3 collisionPoint = collision.GetContact(0).point;
+                SpawnSplash(collisionPoint, collision.gameObject);
+            }
         }
+    }
+
+    private void SpawnSplash(Vector3 position, GameObject platform)
+    {
+        if (splashPrefab != null)
+        {
+            GameObject splash = Instantiate(splashPrefab, position, Quaternion.identity);
+            splash.transform.SetParent(platform.transform);
+            if (splash.transform.localPosition.z < 0.0015f)
+                splash.transform.localPosition += new Vector3(0, 0, 0.002f);
+            else
+                splash.transform.localPosition += new Vector3(0, 0, 0.0012f);
+            splash.transform.localRotation = Quaternion.Euler(180,0,0);
+            splash.transform.localScale = new (0.001f, 0.001f, 0.001f);
+            StartCoroutine(DisappearSplash(splash));
+        }
+    }
+    private IEnumerator DisappearSplash(GameObject splash)
+    {
+        yield return new WaitForSeconds(0.5f);
+        Destroy(splash);
     }
 }
